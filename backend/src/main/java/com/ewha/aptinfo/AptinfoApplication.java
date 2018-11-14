93bb0611023849cb3fd15ac85796733bfd33b376
Jiwon Yeom <jiwony@kr.ibm.com>
2018-11-14 21:46:47 +0900

    initial commit

---
+++ backend/src/main/java/com/ewha/aptinfo/AptinfoApplication.java
@@ -0,0 +1,12 @@
+package com.ewha.aptinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AptinfoApplication.class, args);
+	}
+}
---
+++ backend/src/main/java/com/ewha/aptinfo/controller/AptInfoController.java
@@ -0,0 +1,4 @@
+package com.ewha.aptinfo.controller;
+
+public class AptInfoController {
+}
